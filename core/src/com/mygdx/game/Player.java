841121d0fd45e70d33913f02a7cedc4f221ec05e
adriangc24 <adriangcamacho24@gmail.com>
2020-05-23 14:15:47 +0200

    walking animation

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -1,11 +1,41 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Player {
-    public Player(TextureRegion textureRegonWalking, TextureRegion textureRegionRunning) {
+    private static final int FRAME_COLS = 6;
+    private static final int FRAME_ROWS = 4;
+
+    public static final int WALKING = 0;
+    public static final int RUNNING = 1;
+    private int currentAnimation;
+
+    private float stateTime = 0;
+
+    private Animation<TextureRegion> walkingAnimation;
+    private Animation<TextureRegion> runningAnimation;
+
+    private TextureRegion[] walkingFrames;
+    private TextureRegion[] runningFrames;
+
+    private TextureRegion currentFrame;
+    private Animation[] animations;
+
+    public Player(TextureRegion textureRegionWalking, TextureRegion textureRegionRunning) {
+        TextureRegion[][] tmp = textureRegionWalking.split(textureRegionWalking.getRegionWidth() / FRAME_COLS,
+                textureRegionWalking.getRegionHeight() / FRAME_ROWS);
+        walkingFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkingFrames[index++] = tmp[i][j];
+            }
+        }
+        walkingAnimation = new Animation(0.05f, walkingFrames);
+        walkingAnimation.setPlayMode(Animation.PlayMode.LOOP);
     }
         public void setCurretAnimation(int currentAnimation){
 
